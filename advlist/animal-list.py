4eeaadd02e81c0b194dd1580ca00f274e6b34bc4
Jonathan Reyes <jonathanjoereyes@gmail.com>
2024-08-05 21:44:17 +0000

    optional challenge lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""List - creating a list of animals"""
+
+
+def main():
+    
+    animallist = ["Fox", "Fly", "Fly"," Ant", "Bee", "Cod",  "Cat", "Dog","Yak","Cow","Hen","Koi", "Hog", "Jay", "Kit"]
+
+    print (animallist)
+
+
+main()
